29392bd13aed06e746f5cae780a914a983ebdfdf
ladykemzz <c5024151@shu.ac.uk>
2025-10-28 11:15:31 +0000

    End of lab

--- js/main.js
+++ js/main.js
@@ -8,3 +8,37 @@ const myHeading = document.getElementById("myHeading");
 myHeading.innerText = "Kemi's Site";
 let firstButton = document.querySelector(".btn");
 console.log(firstButton); // Logs the first element with the class 'btn'
+let colourButtons = document.querySelectorAll(".colPicker");
+console.dir(colourButtons); // Logs a NodeList of all elements with the class 'colPicker'
+
+// colourButtons.forEach(button => {
+//     button.style.backgroundColor = "red";
+// });
+
+document.getElementById('myTestBtn').addEventListener('click', myFunction);
+
+function myFunction() {
+  console.info('hi');
+}
+document.getElementById('myTestBtn').addEventListener('click', () => {
+    console.info('Hello')
+    console.info('World')
+  });
+
+
+  const redBtn = document.querySelector(".red")
+redBtn.addEventListener('click', function(){
+    document.body.setAttribute("class", "redBack")
+})
+const greenBtn = document.querySelector(".green")
+greenBtn.addEventListener('click', function(){
+    document.body.setAttribute("class", "greenBack")
+})
+const blueBtn = document.querySelector(".blue")
+blueBtn.addEventListener('click', function(){
+    document.body.setAttribute("class", "blueBack")
+})
+const resetBtn = document.querySelector(".reset")
+resetBtn.addEventListener('click', function(){
+    document.body.removeAttribute("class")
+})
